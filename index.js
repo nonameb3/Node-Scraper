a980ad591fe6d82cadd6b3c99b04c21c7c1dc61c
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-06-11 16:50:12 +0700

    Add file system

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const scrape = require('website-scraper');
-const options = {
-  urls: ['https://madsparrow.me/emily/index.html'],
-  directory: './temp',
+const fs = require('fs-extra');
+
+const path = './temp';
+const urls = ['https://nodejs.org/en/'];
+
+// remove old folder
+if(fs.existsSync(path)) {
+  fs.removeSync(path);
+}
+
+// website-scraper
+const WSoptions = {
+  urls: urls,
+  directory: path,
 };
 
-scrape(options).then((result) => {
+scrape(WSoptions).then(() => {
   console.log('Succesfully downloaded!!');
 }).catch(err => {
   console.log('error', err);
